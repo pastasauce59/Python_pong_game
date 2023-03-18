08cebfb4a9c7e330e176c204606fc834981dff58
Michael Primavera <pastasauce59@gmail.com>
2023-03-18 19:31:34 -0400

    changed time_speed variable to an attribute called move_speed in ball module, set the move speed to increase a little bit faster each time it makes contact on the x axis, set the move speed to be reset to 0.1 speed in the ball_reset method for the when the ball goes out of bounds.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from ball import Ball
 from scoreboard import Scoreboard
 import time
 
-time_speed = 0.1
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor('black')
@@ -24,7 +23,7 @@ screen.onkey(l_paddle.move_down, 's')
 
 game_is_on = True
 while game_is_on:
-    time.sleep(time_speed)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -35,22 +34,16 @@ while game_is_on:
     #Detect collision with paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
-        if time_speed == 0:
-            pass
-        else:
-            time_speed -= 0.01
     
     #Detect if right paddle misses
     if ball.xcor() > 390:
         ball.ball_reset()
         scoreboard.increase_score('left')
-        time_speed = 0.1
 
     #Detect if left paddle misses
     if ball.xcor() < -390:
         ball.ball_reset()
         scoreboard.increase_score('right')
-        time_speed = 0.1
 
     #Detect winner
     if scoreboard.declare_winner():
